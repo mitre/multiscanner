5cfbf40646fad8e109d9f377a6518e250bfcba0a
Drew Bonasera <Drewsif@users.noreply.github.com>
2015-12-07 15:27:06 -0500

    TEST ALL THE THINGS!!! But really only some of the things...

---
+++ tests/modules/test_1.py
@@ -0,0 +1,20 @@
+"""
+A test module which requires no config and the result is the filename
+"""
+TYPE = "Test"
+NAME = "test_1"
+
+def check():
+    return True
+
+def scan(filelist):
+    results = []
+
+    for fname in filelist:
+        results.append((fname, fname))
+
+    metadata = {}
+    metadata["Name"] = NAME
+    metadata["Type"] = TYPE
+    metadata["Include"] = False
+    return results, metadata
