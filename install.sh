2315e0a1459543ec02a5b50a63380f0455073924
Drew Bonasera <Drewsif@users.noreply.github.com>
2015-12-08 16:25:42 -0500

    Add comments to install.sh

--- install.sh
+++ install.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
+#This script attempts to install the required packages for MultiScanner and its modules
+
+#Install requirements for Redhat derivatives
 if [ -e /etc/redhat-release ]; then
   yum install -y epel-release
   yum install -y autoconf automake curl gcc libffi-devel libtool make python-devel python-pip ssdeep-devel tar
 fi
 
+#Install requirements for Debian derivatives
 if [ -e /etc/debian_version ]; then
   apt-get update
   apt-get install -y build-essential curl dh-autoreconf gcc libffi-dev libfuzzy-dev python-dev python-pip
 fi
 
+#Install requirements for Python 2.x
 if [[ $(python -V 2>&1) == Python\ 2* ]]; then
   pip install -r requirements2.txt
 fi
 
+#Install requirements for Python 3.x
 if [[ $(python -V 2>&1) == Python\ 3* ]]; then
   pip install -r requirements3.txt
 fi
 
 CWD=`pwd`
+#Code to compile and install yara
 read -p "Compile yara 3.4.0? <y/N> " prompt
 if [[ $prompt == "y" ]]; then
   cd /tmp
