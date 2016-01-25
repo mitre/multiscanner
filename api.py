68c7136105027918d19906ac960616b1e92f568f
Austin West <awest1339@gmail.com>
2016-01-25 11:23:45 -0700

    Make POST accept a file

--- api.py
+++ api.py
@@ -9,13 +9,18 @@ GET /api/v1/tasks/list  ---> Receive list of tasks in multiscanner
 GET /api/v1/tasks/<task_id> ---> receive report in JSON format
 GET /api/v1/tasks/delete/<task_id> ----> delete task_id
 POST /api/v1/tasks/create ---> POST file and receive report id
+Sample POST usage:
+    curl -i -X POST http://localhost:8080/api/v1/tasks/create/ -F file=@/bin/ls
 
 TODO:
+* POST to /api/v1/tasks/create should accept a file
 * Add a backend DB to store reports
 * Add doc strings to functions
 '''
-
+import os
+import uuid
 from flask import Flask, jsonify, make_response, request, abort
+import multiscanner
 
 TASKS = [
     {'id': 1, 'report': {"/tmp/example.log":{"MD5":"53f43f9591749b8cae536ff13e48d6de","SHA256":"815d310bdbc8684c1163b62f583dbaffb2df74b9104e2aadabf8f8491bafab66","libmagic":"ASCII text"}}},
@@ -24,6 +29,7 @@ TASKS = [
 
 TASK_NOT_FOUND = {'Message': 'No task with that ID not found!'}
 INVALID_REQUEST = {'Message': 'Invalid request parameters'}
+UPLOAD_FOLDER = 'tmp/'
 
 HTTP_OK = 200
 HTTP_CREATED = 201
@@ -36,6 +42,7 @@ app = Flask(__name__)
 @app.errorhandler(HTTP_BAD_REQUEST)
 def invalid_request(error):
     return make_response(jsonify(INVALID_REQUEST), HTTP_BAD_REQUEST)
+
 
 @app.errorhandler(HTTP_NOT_FOUND)
 def not_found(error):
@@ -71,14 +78,21 @@ def delete_task(task_id):
 
 @app.route('/api/v1/tasks/create/', methods=['POST'])
 def create_task():
-    if not request.json or not 'report' in request.json:
-        abort(HTTP_BAD_REQUEST)
-    task = {
-        'id': TASKS[-1]['id'] + 1,
-        'report': request.json['report'],
-    }
-    TASKS.append(task)
-    return jsonify({'Message': 'Added'}), HTTP_CREATED
+    file_ = request.files['file']
+    extension = os.path.splitext(file_.filename)[1]
+    f_name = str(uuid.uuid4()) + extension
+    file_path = os.path.join(UPLOAD_FOLDER, f_name)
+    file_.save(file_path)
+    # TODO: save file to DB
+    # TODO: run multiscan on the file, have it update the
+    # DB when done
+    # output = multiscanner.multiscan([file_path])
+    # report = multiscanner.parseReports
+    
+
+    id = TASKS[-1]['id'] + 1
+    TASKS.append({'id': id, 'report': 'pending'})
+    return make_response(jsonify({'Message': {'task_id': id}}), HTTP_CREATED)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=False)
+    app.run(host='0.0.0.0', port=8080, debug=True)
