544bc6383141cb786a06bd4a975940c6e52e2d11
Austin West <awest1339@gmail.com>
2016-01-29 15:47:35 -0700

    Add storage proof of concept files

---
+++ storage/elasticsearch_storage.py
@@ -0,0 +1,23 @@
+from storage import Storage
+
+class ElasticSearchStorage(Storage):
+    def __init__(self, config_dict):
+        self.db = config_dict['database']
+        self.host = config_dict['host']
+        self.port = config_dict['port']
+        self.username = config_dict['username']
+        self.password = config_dict['password']
+        self.index = config_dict['index']
+        self.doc_type = config_dict['doc_type']
+
+    def store(self, task):
+        return 'Task ID'
+
+    def get(self, task_id):
+        return {1: {'report': 'data'}}
+
+    def list(self):
+        return [{1: {'report': 'data'}}]
+
+    def delete(self, task_id):
+        return {'Message': 'deleted'}
