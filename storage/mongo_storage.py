583df1bb01d63564ebd86bb56758fe8fe9a52bb3
Austin West <awest1339@gmail.com>
2016-02-29 16:15:44 -0700

    Change storage framework to refer to reports instead of tasks

--- storage/mongo_storage.py
+++ storage/mongo_storage.py
@@ -10,14 +10,11 @@ class MongoStorage(Storage):
         self.index = config_dict['index']
         self.doc_type = config_dict['doc_type']
 
-    def store(self, task):
-        return 'Task ID'
+    def store(self, report):
+        return 'Report ID'
 
-    def get_task(self, task_id):
-        return {2: {'task': 'data'}}
-
-    def get_report(self, task_id):
+    def get_report(self, report_id):
         return {2: {'report': 'data'}}
 
-    def delete(self, task_id):
+    def delete(self, report_id):
         return {'Message': 'deleted'}
