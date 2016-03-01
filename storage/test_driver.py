c8f8572ba40ad47f3b983fa90861a7dae04aef05
Austin West <awest1339@gmail.com>
2016-03-01 13:08:27 -0700

    Update test driver and docs

--- storage/test_driver.py
+++ storage/test_driver.py
@@ -2,15 +2,21 @@
 
 from storage import Storage
 
-# STORAGE_CONFIG = '/opt/multiscanner/config_storage.ini'
+NEW_REPORT = {'foo': 'bar', 'boo': 'baz'}
 
 def main():
     db_store = Storage.get_storage()
     for key, value in db_store.__dict__.iteritems():
         print '%s: %s' % (key, value)
-
     print '\n'
-    print db_store.get_report(1)
+
+    db_store.store(NEW_REPORT)
+
+    print db_store.get_report(2)
+    print db_store.get_report(3)
+
+    db_store.delete(2)
+    print db_store.delete(2)
 
 
 if __name__ == '__main__':
