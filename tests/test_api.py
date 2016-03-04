284663da352cc5063dee62a4b2177f316be02a2e
Austin West <awest1339@gmail.com>
2016-03-04 14:47:04 -0700

    Add test for delete calls

--- tests/test_api.py
+++ tests/test_api.py
@@ -26,9 +26,6 @@ from sqlite_driver import Database
 TEST_DB_PATH = os.path.join(CWD, 'testing.db')
 TEST_UPLOAD_FOLDER = os.path.join(CWD, 'tmp')
 
-HTTP_OK = 200
-HTTP_CREATED = 201
-
 
 def post_file(app):
     return app.post(
@@ -52,19 +49,19 @@ class TestURLCase(unittest.TestCase):
     def test_index(self):
         expected_response = {'Message': 'True'}
         resp = self.app.get('/')
-        self.assertEqual(resp.status_code, HTTP_OK)
+        self.assertEqual(resp.status_code, api.HTTP_OK)
         self.assertEqual(json.loads(resp.data), expected_response)
 
     def test_empty_db(self):
         expected_response = {'Tasks': []}
         resp = self.app.get('/api/v1/tasks/list/')
-        self.assertEqual(resp.status_code, HTTP_OK)
+        self.assertEqual(resp.status_code, api.HTTP_OK)
         self.assertEqual(json.loads(resp.data), expected_response)
 
     def test_create_first_task(self):
         expected_response = {'Message': {'task_id': 1}}
         resp = post_file(self.app)
-        self.assertEqual(resp.status_code, HTTP_CREATED)
+        self.assertEqual(resp.status_code, api.HTTP_CREATED)
         self.assertEqual(json.loads(resp.data), expected_response)
 
     def tearDown(self):
@@ -96,7 +93,51 @@ class TestTaskCreateCase(unittest.TestCase):
             }
         }
         resp = self.app.get('/api/v1/tasks/list/1')
-        self.assertEqual(resp.status_code, HTTP_OK)
+        self.assertEqual(resp.status_code, api.HTTP_OK)
+        self.assertDictEqual(json.loads(resp.data), expected_response)
+
+    def test_get_nonexistent_task(self):
+        expected_response = api.TASK_NOT_FOUND
+        resp = self.app.get('/api/v1/tasks/list/2')
+        self.assertEqual(resp.status_code, api.HTTP_NOT_FOUND)
+        self.assertDictEqual(json.loads(resp.data), expected_response)
+
+    def test_get_task_list(self):
+        expected_response = {'Tasks': [{'report_id': None, 'task_id': 1, 'task_status': 'Pending'}]}
+        resp = self.app.get('/api/v1/tasks/list/')
+        self.assertEqual(resp.status_code, api.HTTP_OK)
+        self.assertDictEqual(json.loads(resp.data), expected_response)
+
+    def tearDown(self):
+        # Clean up Test DB and upload folder
+        os.remove(TEST_DB_PATH)
+        shutil.rmtree(TEST_UPLOAD_FOLDER)
+
+
+class TestTaskDeleteCase(unittest.TestCase):
+    def setUp(self):
+        self.sql_db = Database(TEST_DB_PATH)
+        self.sql_db.init_sqlite_db()
+        self.app = api.app.test_client()
+        # Replace the real production DB w/ a testing DB
+        api.db = self.sql_db
+        api.UPLOAD_FOLDER = TEST_UPLOAD_FOLDER
+        if not os.path.isdir(api.UPLOAD_FOLDER):
+            os.makedirs(api.UPLOAD_FOLDER)
+
+        # populate the DB w/ a task
+        post_file(self.app)
+
+    def test_delete_task(self):
+        expected_response = {'Message': 'Deleted'}
+        resp = self.app.get('/api/v1/tasks/delete/1')
+        self.assertEqual(resp.status_code, api.HTTP_OK)
+        self.assertDictEqual(json.loads(resp.data), expected_response)
+
+    def test_delete_nonexistent_task(self):
+        expected_response = api.TASK_NOT_FOUND
+        resp = self.app.get('/api/v1/tasks/delete/2')
+        self.assertEqual(resp.status_code, api.HTTP_NOT_FOUND)
         self.assertDictEqual(json.loads(resp.data), expected_response)
 
     def tearDown(self):
