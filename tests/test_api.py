293e027fecc03c4dbc778290bfea63c518d0d441
Austin West <awest1339@gmail.com>
2016-03-04 13:55:09 -0700

    Test initial file upload

--- tests/test_api.py
+++ tests/test_api.py
@@ -3,6 +3,7 @@ import sys
 import json
 import responses
 import unittest
+from StringIO import StringIO
 
 CWD = os.path.dirname(os.path.abspath(__file__))
 MS_WD = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -21,6 +22,7 @@ from sqlite_driver import Database
 
 
 TEST_DB_PATH = os.path.join(CWD, 'testing.db')
+TEST_UPLOAD_FOLDER = os.path.join(CWD, 'tmp')
 
 HTTP_OK = 200
 HTTP_CREATED = 201
@@ -33,6 +35,10 @@ class TestURLCase(unittest.TestCase):
         self.app = api.app.test_client()
         # Replace the real production DB w/ a testing DB
         api.db = self.sql_db
+        api.UPLOAD_FOLDER = TEST_UPLOAD_FOLDER
+        if not os.path.isdir(api.UPLOAD_FOLDER):
+            os.makedirs(api.UPLOAD_FOLDER)
+        
 
     def test_index(self):
         expected_response = {'Message': 'True'}
@@ -44,6 +50,16 @@ class TestURLCase(unittest.TestCase):
         expected_response = {'Tasks': []}
         resp = self.app.get('/api/v1/tasks/list/')
         self.assertEqual(resp.status_code, HTTP_OK)
+        self.assertEqual(json.loads(resp.data), expected_response)
+
+    def test_create_first_task(self):
+        expected_response = {'Message': {'task_id': 1}}
+        resp = self.app.post(
+            '/api/v1/tasks/create/',
+            data={
+                'file': (StringIO('my file contents'), 'hello world.txt'),}
+        )
+        self.assertEqual(resp.status_code, HTTP_CREATED)
         self.assertEqual(json.loads(resp.data), expected_response)
 
     def tearDown(self):
