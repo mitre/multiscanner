a6b9d95103fd9c2909f5cb3cdf0d7e0743f5f65f
Austin West <awest1339@gmail.com>
2016-03-04 12:31:07 -0700

    Disable debug for api

--- utils/api.py
+++ utils/api.py
@@ -158,4 +158,4 @@ if __name__ == '__main__':
     if not os.path.isdir(UPLOAD_FOLDER):
         print 'Creating upload dir'
         os.makedirs(UPLOAD_FOLDER)
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080)
