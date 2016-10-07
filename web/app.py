5d00c4f063cbe4a6365e171baa8b168dfcb867e9
clenk <clenk@mitre.org>
2016-10-07 14:10:57 -0400

    Pull from API instead of hardcoding data

--- web/app.py
+++ web/app.py
@@ -1,32 +1,26 @@
-import argparse
 from flask import (Flask, render_template, request, redirect, url_for, make_response, flash)
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html', methods=['GET'])
+    return render_template('index.html', api_loc=app.config['API_LOC'])
 
 
-@app.route('/tasks')
+@app.route('/tasks', methods=['GET'])
 def tasks():
-    return render_template('tasks.html', methods=['GET'])
+    return render_template('tasks.html', api_loc=app.config['API_LOC'])
 
 
 @app.route('/report/<int:task_id>', methods=['GET'])
 def reports(task_id=1):
-    return render_template('report.html', task_id=task_id)
+    return render_template('report.html', task_id=task_id,
+                           api_loc=app.config['API_LOC'])
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-H", "--host", help="Host to bind the API server on",
-                        default="localhost", action="store", required=False)
-    parser.add_argument("-p", "--port", help="Port to bind the API server on",
-                        default=8000, action="store", required=False)
-    parser.add_argument("-d", "--debug", action="store_true",
-                        help="Enable debug mode (NOT for production!)")
-    args = parser.parse_args()
-
-    app.run(debug=args.debug, port=args.port, host=args.host)
+    app.config.from_object('config')
+    app.run(debug=app.config['DEBUG'],
+            port=app.config['PORT'],
+            host=app.config['HOST'])
