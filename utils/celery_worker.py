69a6ef0135d2fe6db5fde3fdd9400e4478b7f577
Austin West <awest@mgmt.xbis>
2017-03-13 13:42:52 -0400

    Update task DB when the worker is finished

--- utils/celery_worker.py
+++ utils/celery_worker.py
@@ -1,8 +1,13 @@
 import os
 import sys
+MS_WD = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Append .. to sys path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+# Add the storage dir to the sys.path. Allows import of sql_driver module
+if os.path.join(MS_WD, 'storage') not in sys.path:
+    sys.path.insert(0, os.path.join(MS_WD, 'storage'))
 import multiscanner
+import sql_driver as database
 
 from celery import Celery
 
@@ -10,12 +15,12 @@ RABBIT_USER = 'guest'
 RABBIT_HOST = 'localhost'
 
 app = Celery('celery_worker', broker='pyamqp://%s@%s//' % (RABBIT_USER, RABBIT_HOST))
+db = database.Database()
 
 @app.task
-def multiscanner_celery(filelist, config=multiscanner.CONFIG):
+def multiscanner_celery(file_, original_filename, task_id, file_hash, config=multiscanner.CONFIG):
     '''
     TODO: Figure out how to do batching.
-    TODO: Add other ars + config options...
     This function essentially takes in a file list and runs
     multiscanner on them. Results are stored in the
     storage configured in storage.ini.
@@ -24,12 +29,30 @@ def multiscanner_celery(filelist, config=multiscanner.CONFIG):
     from celery_worker import multiscanner_celery
     multiscanner_celery.delay([list, of, files, to, scan])
     '''
+    # Initialize the connection to the task DB
+    db.init_db()
+
+    # Get the storage config
     storage_conf = multiscanner.common.get_storage_config_path(config)
     storage_handler = multiscanner.storage.StorageHandler(configfile=storage_conf)
 
-    resultlist = multiscanner.multiscan(filelist, configfile=config)
+    resultlist = multiscanner.multiscan([file_], configfile=config)
     results = multiscanner.parse_reports(resultlist, python=True)
+    # Use the original filename as the value for the filename
+    # in the report (instead of the tmp path assigned to the file
+    # by the REST API)
+    results[original_filename] = results[file_]
+    del results[file_]
 
+    # Save the report to storage
     storage_handler.store(results, wait=False)
     storage_handler.close()
+
+    # Update the task DB to reflect that the task is done
+    db.update_task(
+        task_id=task_id,
+        task_status='Complete',
+        report_id=file_hash
+    )
+
     return results
