92941b58fa07ec176e6eb758192b1359085e2dc5
clenk <clenk@mitre.org>
2017-05-11 13:46:28 -0400

    Fix tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -3,7 +3,7 @@ import shutil
 import sys
 import json
 try:
-    from StringIO import StringIO
+    from StringIO import StringIO as BytesIO
 except:
     from io import BytesIO
 import unittest
@@ -111,7 +111,8 @@ class TestTaskCreateCase(unittest.TestCase):
             'Task': {
                 'task_id': 1,
                 'task_status': 'Pending',
-                'report_id': None
+                'report_id': None,
+                'sample_id': '114d70ba7d04c76d8c217c970f99682025c89b1a6ffe91eb9045653b4b954eb9'
             }
         }
         resp = self.app.get('/api/v1/tasks/list/1')
@@ -125,7 +126,12 @@ class TestTaskCreateCase(unittest.TestCase):
         self.assertDictEqual(json.loads(resp.get_data()), expected_response)
 
     def test_get_task_list(self):
-        expected_response = {'Tasks': [{'task_id': 1, 'task_status': 'Pending', 'report_id': None}]}
+        expected_response = {'Tasks': [{
+            'task_id': 1,
+            'task_status': 'Pending',
+            'report_id': None,
+            'sample_id': '114d70ba7d04c76d8c217c970f99682025c89b1a6ffe91eb9045653b4b954eb9'
+        }]}
         resp = self.app.get('/api/v1/tasks/list/')
         self.assertEqual(resp.status_code, api.HTTP_OK)
         self.assertDictEqual(json.loads(resp.get_data()), expected_response)
@@ -159,7 +165,8 @@ class TestTaskUpdateCase(unittest.TestCase):
             'Task': {
                 'task_id': 1,
                 'task_status': 'Complete',
-                'report_id': 'report1'
+                'report_id': 'report1',
+                'sample_id': '114d70ba7d04c76d8c217c970f99682025c89b1a6ffe91eb9045653b4b954eb9'
             }
         }
         resp = self.app.get('/api/v1/tasks/list/1')
