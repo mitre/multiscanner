0444695eed54f353f75ec93d28019c7f365c4cfb
Emmanuelle Vargas-Gonzalez <emmanuelle@mitre.org>
2017-10-10 10:15:13 -0400

    Code refinement. Change quotes

--- modules/Metadata/flarefloss.py
+++ modules/Metadata/flarefloss.py
@@ -5,22 +5,22 @@ from __future__ import division, absolute_import, with_statement, print_function
 import os
 import subprocess
 
-__author__ = "Emmanuelle Vargas-Gonzalez"
-__license__ = "MPL 2.0"
+__author__ = 'Emmanuelle Vargas-Gonzalez'
+__license__ = 'MPL 2.0'
 
-TYPE = "Metadata"
-NAME = "floss"
+TYPE = 'Metadata'
+NAME = 'floss'
 DEFAULTCONF = {
-    "ENABLED": False,
-    "path": "C:\\floss64.exe",
-    "cmdline": [u"--show-metainfo"]
+    'ENABLED': False,
+    'path': 'C:\\floss64.exe',
+    'cmdline': [u'--show-metainfo']
 }
 
 
 def check(conf=DEFAULTCONF):
-    if not conf["ENABLED"]:
+    if not conf['ENABLED']:
         return False
-    if os.path.isfile(conf["path"]):
+    if os.path.isfile(conf['path']):
         return True
     else:
         return False
@@ -32,58 +32,50 @@ def scan(filelist, conf=DEFAULTCONF):
     for fname in filelist:
         ret = {}
         cmd = _build_command(conf, fname)
-        p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
 
-        for f in p.stdout:
-            f = f.decode("utf-8")
-            if u"FLOSS static ASCII strings" in f:
-                ret["static_ascii_strings"] = []
-                f = next(p.stdout, "").decode("utf-8")
-                while f != "":
-                    ret["static_ascii_strings"].append(f)
-                    f = next(p.stdout, "").decode("utf-8")
-                    f = f.strip()
-            elif u"FLOSS static UTF-16 strings" in f:
-                ret["static_utf16_strings"] = []
-                f = next(p.stdout, "").decode("utf-8")
-                while f != "":
-                    ret["static_utf16_strings"].append(f)
-                    f = next(p.stdout, "").decode("utf-8")
-                    f = f.strip()
-            elif u"FLOSS decoded" in f:
-                ret["decoded_strings"] = []
-                f = next(p.stdout, "").decode("utf-8")
-                while f != "":
-                    ret["decoded_strings"].append(f)
-                    f = next(p.stdout, "").decode("utf-8")
-                    f = f.strip()
-            elif u"stackstrings" in f:
-                ret["stack_strings"] = []
-                f = next(p.stdout, "").decode("utf-8")
-                while f != "":
-                    ret["stack_strings"].append(f)
-                    f = next(p.stdout, "").decode("utf-8")
-                    f = f.strip()
-            elif u"Vivisect workspace analysis information" in f:
-                ret["vivisect_meta_info"] = []
-                f = next(p.stdout, "").decode("utf-8")
-                while f != "":
-                    ret["vivisect_meta_info"].append(f)
-                    f = next(p.stdout, "").decode("utf-8")
-                    f = f.strip()
+        try:
+            p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+
+            for f in p.stdout:
+                f = f.decode('utf-8')
+                if u'FLOSS static ASCII strings' in f:
+                    _extract_data(p.stdout, ret, 'static_ascii_strings')
+                elif u'FLOSS static UTF-16 strings' in f:
+                    _extract_data(p.stdout, ret, 'static_utf16_strings')
+                elif u'stackstrings' in f:
+                    _extract_data(p.stdout, ret, 'stack_strings')
+                elif u'Vivisect workspace analysis information' in f:
+                    _extract_data(p.stdout, ret, 'vivisect_meta_info')
+        except subprocess.CalledProcessError as e:
+            print(e.message)
+            print(e.returncode)
 
         if ret:
             results.append((fname, ret))
 
     metadata = {}
-    metadata["Name"] = NAME
-    metadata["Type"] = TYPE
-    metadata["Include"] = False
+    metadata['Name'] = NAME
+    metadata['Type'] = TYPE
+    metadata['Include'] = False
     return (results, metadata)
 
 
 def _build_command(conf, fname):
-    cmd = [conf["path"], fname]
-    cmd.extend(conf["cmdline"])
+    cmd = [conf['path'], fname]
+    cmd.extend(conf['cmdline'])
 
     return cmd
+
+
+def _extract_data(out, ret, key):
+    """Sub-routine to extract fragment of console output"""
+    ret[key] = []
+    feed = next(out, u'').decode('utf-8')
+    feed = feed.strip()
+    while feed != u'':
+        ret[key].append(feed)
+        feed = next(out, u'').decode('utf-8')
+        feed = feed.strip()
+
+    if not ret[key]:
+        del ret[key]
