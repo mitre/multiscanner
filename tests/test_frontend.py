c873c3e074692a7f1e162278a1a1a0e5783984f8
Patrick Copeland <pcopeland@mitre.org>
2018-02-06 11:48:38 -0500

    Add addtional frontend tests
    Notes:
    - Submission tests only working with Firefox
      - After POST, Chrome receives a 201 from the server immediately followed by RST,ACK
    - Would like to get rid of XPATH references
    - Would like a way to intercept and inspect  actual POST msg sent from web UI

--- tests/test_frontend.py
+++ tests/test_frontend.py
@@ -1,10 +1,15 @@
-import os
-import sys
 import json
+import os
 import requests
+import sys
+import time
 
-from flask import Flask
+from flask import Flask, url_for
 from flask_testing import LiveServerTestCase
+from mocks import get_free_server_port, start_mock_server
+from pathlib import Path
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 CWD = os.path.dirname(os.path.abspath(__file__))
 MS_WD = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -26,16 +31,142 @@ proxies = {
   "https": None,
 }
 
-class MyTest(LiveServerTestCase):
+test_submitter_name = 'John Doe'
+test_submitter_email = 'jdoe@local.domain'
+test_submitter_org = 'Testers'
+test_submitter_phone = '123-456-7890'
+test_submission_desc = 'A test document submission'
+
+
+class TestBase(LiveServerTestCase):
+
+    @classmethod
+    def setup_class(cls):
+        cls.mock_server_port = get_free_server_port()
+        # cls.mock_server_port = 8080
+        start_mock_server(cls.mock_server_port)
 
     def create_app(self):
         app = flask_app
         app.config.update(
             TESTING=True,
+            API_LOC='http://localhost:{}'.format(self.mock_server_port),
             LIVESERVER_PORT=8943,
             LIVESERVER_TIMEOUT=10)
         return app
 
+    def setUp(self):
+        """Set up test driver"""
+        #self.driver = webdriver.Chrome()
+        self.driver = webdriver.Firefox()
+        self.driver.get(self.get_server_url())
+
+    def tearDown(self):
+        self.driver.quit()
+
     def test_is_server_up(self):
         resp = requests.get(self.get_server_url(), proxies=proxies)
         self.assertEqual(resp.status_code, 200)
+
+
+class TestSubmission(TestBase):
+
+
+    def _add_files(self, files):
+        for fname in files:
+            data_file = Path(__file__).parent / 'test_files/{}'.format(fname)
+            assert data_file.exists()
+
+            # this is the drag-and-drop box (maybe more of our HTML elements should
+            # have ids)
+            self.driver.find_element(
+                By.XPATH,
+                '/html/body/div[1]/div[2]/div/form/fieldset/div[1]/div/div/div[1]/div[2]'
+            ).send_keys(str(data_file))
+
+            # send file path
+            self.driver.find_element_by_css_selector("input[type=\"file\"]").clear()
+            self.driver.find_element_by_css_selector("input[type=\"file\"]").send_keys(str(data_file))
+
+            # find file upload button
+            # Note: Firefox doesn't like this...not actually the btn's id
+            # self.driver.find_element_by_id('filesUpload').click()
+
+    def _successful_submission(self):
+        val = self.driver.find_element(By.CLASS_NAME, 'progress-bar')
+        return 'Submitted' in val.text
+
+    def test_single_file_submission(self):
+        """Test that a user is able to submit a single file.
+        """
+        self._add_files(['1.txt'])
+
+        # submit file
+        self.driver.find_element_by_id('btn-scan').click()
+
+        assert self._successful_submission()
+
+    def test_multiple_file_submission(self):
+        """Test that a user is able to submit multiple files.
+        """
+        self._add_files(['1.txt', '2.txt', '3.txt'])
+
+        # submit files
+        self.driver.find_element_by_id('btn-scan').click()
+
+        assert self._successful_submission()
+
+    def test_submission_with_metadata_fields(self):
+        """Test that a user is able to submit file with metadata fields.
+        """
+        self._add_files(['1.txt'])
+
+        self.driver.find_element_by_id('btn-opts').click()
+        time.sleep(1)
+        self.driver.find_element_by_id(
+            'metadata-Submitter-Name').send_keys(test_submitter_name)
+        self.driver.find_element_by_id(
+            'metadata-Submission-Description').send_keys(test_submission_desc)
+        self.driver.find_element_by_id(
+            'metadata-Submitter-Email').send_keys(test_submitter_email)
+        self.driver.find_element_by_id(
+            'metadata-Submitter-Organization').send_keys(test_submitter_org)
+        self.driver.find_element_by_id(
+            'metadata-Submitter-Phone').send_keys(test_submitter_phone)
+
+        # submit files
+        self.driver.find_element_by_id('btn-scan').click()
+
+        assert self._successful_submission()
+
+    def test_help_button(self):
+        """Test that the help button opens diaglog box.
+        """
+
+        # find help display and make sure it's hidden
+        element = self.driver.find_element_by_id('help-modal')
+        assert not(element.is_displayed())
+
+        # click the help icon
+        self.driver.find_element(
+            By.XPATH,
+            '/html/body/nav/div/div[2]/ul[2]/li[2]').click()
+
+        # give it a second to open and then verify it's open
+        time.sleep(1)
+        element = self.driver.find_element_by_id('help-modal')
+        self.assertEqual(element.get_property('hidden'), False)
+        assert element.is_displayed()
+
+    def test_change_tabs(self):
+        """Test that the navigation tabs work.
+        """
+        self.driver.find_element(
+            By.XPATH,
+            '/html/body/nav/div/div[2]/ul[1]/li[2]/a').click()
+        time.sleep(1)
+        assert url_for('tasks') in self.driver.current_url
+
+    #def test_search_functionality(self):
+    #    """Test something about the search functionality.
+    #    """
