a97eb7da75f87ba0bdd4bd9800dd30e04cff155e
awest1339 <awest1339@users.noreply.github.com>
2018-02-06 20:07:59 -0700

    Remove unused json import

--- modules/MachineLearning/MaliciousMacroBot.py
+++ modules/MachineLearning/MaliciousMacroBot.py
@@ -2,7 +2,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 from __future__ import division, absolute_import, with_statement, print_function, unicode_literals
-import json
 
 __authors__ = "Austin West"
 __license__ = "MPL 2.0"
