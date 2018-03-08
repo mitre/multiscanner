4eff14986eca436eb1c05d0029773dd65569c603
Emmanuelle Vargas-Gonzalez <emmanuelle@mitre.org>
2018-03-08 10:41:25 -0500

    Initial test stubs

---
+++ tests/utils/stix2_generator/test_stix2_generator.py
@@ -0,0 +1,32 @@
+
+
+def test_create_empty_bundle():
+    pass
+
+
+def test_create_non_empty_bundle():
+    pass
+
+
+def test_join_comparison_expression():
+    pass
+
+
+def test_create_comparison_expression():
+    pass
+
+
+def test_create_observation_expression():
+    pass
+
+
+def test_extract_file_cuckoo():
+    pass
+
+
+def test_extract_http_requests_cuckoo():
+    pass
+
+
+def test_parse_json_report_to_stix2_bundle():
+    pass
