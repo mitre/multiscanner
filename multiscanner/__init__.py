381bb747a3b88d4fed35b71e02ac83df8e527aca
Patrick Copeland <pcopeland@mitre.org>
2018-03-30 14:07:35 -0400

    Make package version to module

--- multiscanner/__init__.py
+++ multiscanner/__init__.py
@@ -5,6 +5,8 @@ sys.path.insert(0, os.path.dirname(__file__))
 
 from . import multiscanner
 
+__version__ = '1.1.1'
+
 common = multiscanner.common
 multiscan = multiscanner.multiscan
 parse_reports = multiscanner.parse_reports
