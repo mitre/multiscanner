28aff7f656cb661a184d0841c8629e4ee27f3e7d
Patrick Copeland <pcopeland@mitre.org>
2018-04-02 15:00:20 -0400

    Update flake8 stuff

--- multiscanner/__init__.py
+++ multiscanner/__init__.py
@@ -2,11 +2,11 @@
 
 __version__ = '1.1.1'
 
-from multiscanner.config import (
+from multiscanner.config import (               # noqa F401
     PY3, raw_input, MS_WD, CONFIG, MODULEDIR
 )
 
-from multiscanner.multiscanner import (
-    config_init, multiscan, parse_reports, CONFIG, MODULEDIR, _ModuleInterface,
-    _GlobalModuleInterface, _Thread, _run_module, _main, MS_WD
+from multiscanner.multiscanner import (         # noqa F401
+    config_init, multiscan, parse_reports, _ModuleInterface,
+    _GlobalModuleInterface, _Thread, _run_module, _main
 )
