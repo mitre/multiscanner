9fdae5ea98abaa5b1c67c4f042c978a7cb910a92
awest1339 <awest1339@users.noreply.github.com>
2018-05-16 13:25:40 -0600

    Remove batch options from api.py

--- utils/api.py
+++ utils/api.py
@@ -164,8 +164,6 @@ except KeyError:
     cors_origins = DEFAULTCONF['cors']
 CORS(app, origins=cors_origins)
 
-batch_size = api_config['api']['batch_size']
-batch_interval = api_config['api']['batch_interval']
 # Add `delete_after_scan = True` to api_config.ini to delete samples after scan has completed
 delete_after_scan = api_config['api'].get('delete_after_scan', False)
 
